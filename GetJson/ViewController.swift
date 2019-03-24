63a808984b41dabed900d99fb16635389c86f6b7
sss123 <sss123@sss123s-mac.local>
2019-03-24 00:36:46 -0700

    Initial Commit

---
+++ GetJson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetJson
+//
+//  Created by sss123 on 3/24/19.
+//  Copyright © 2019 sss123. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
